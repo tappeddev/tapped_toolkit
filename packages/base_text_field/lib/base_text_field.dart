88cf9cdb248c370012c25f63071125b3977830b9
Julian Bissekkou <julian.bissekkou@tapped.dev>
2022-09-28 14:37:08 +0200

    add base_text_field package

---
+++ packages/base_text_field/lib/base_text_field.dart
@@ -0,0 +1,3 @@
+library base_text_field;
+
+export 'src/base_text_field.dart';
---
+++ packages/base_text_field/lib/src/base_text_field.dart
@@ -0,0 +1,214 @@
+import 'dart:async';
+
+import 'package:after_first_build/after_first_build.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+typedef TextStyleMutation = TextStyle Function(TextStyle style);
+
+class BaseTextField extends StatefulWidget {
+  final String text;
+
+  final ValueChanged<String> onChanged;
+  final String? Function(String? value)? validator;
+  final TextStyle Function(TextStyle)? textStyleMutator;
+  final TextStyle textStyle;
+  final bool autoValidate;
+  final bool autofocus;
+
+  final int minLines;
+  final int maxLines;
+
+  final int? maxLength;
+
+  final TextInputType? textInputType;
+
+  final InputDecoration decoration;
+
+  /// [AutofillHints]
+  /// e.g. [AutofillHints.email]
+  final Iterable<String>? autofillHints;
+
+  final ValueChanged<bool>? onFocusChanged;
+
+  final FocusNode? focusNode;
+
+  /// Returns if the TextField is valid or contains an error
+  final ValueChanged<bool>? onValidationChanged;
+
+  final TextInputAction? textInputAction;
+
+  final VoidCallback? onLeave;
+
+  final List<TextInputFormatter>? inputFormatter;
+
+  /// https://stackoverflow.com/questions/63690311/flutter-textfield-difference-between-onedittingcomplete-and-onsubmionsubmittedtted
+  /// Called when the user tap the "continue" button on the keyboard
+  /// When there is no method provided (or the method/callback is null), the keyboard will be closed
+  final VoidCallback? onEditingComplete;
+  final bool obscureText;
+
+  const BaseTextField({
+    required this.text,
+    required this.onChanged,
+    required this.decoration,
+    required this.textStyle,
+    this.onValidationChanged,
+    this.textInputType,
+    this.focusNode,
+    this.validator,
+    this.textStyleMutator,
+    this.maxLength,
+    this.autoValidate = false,
+    this.autofocus = false,
+    this.minLines = 1,
+    this.maxLines = 1,
+    this.onFocusChanged,
+    this.textInputAction,
+    this.autofillHints,
+    this.onLeave,
+    this.onEditingComplete,
+    this.obscureText = false,
+    this.inputFormatter,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  BaseTextFieldState createState() => BaseTextFieldState();
+}
+
+class BaseTextFieldState extends State<BaseTextField>
+    with AfterFirstBuildMixin {
+  late final _focusNode = widget.focusNode ?? FocusNode();
+
+  late TextEditingController _textEditingController;
+
+  @visibleForTesting
+  TextEditingController get textEditingController => _textEditingController;
+
+  @visibleForTesting
+  FocusNode get focusNode => _focusNode;
+
+  bool _textFieldIsValid = true;
+
+  @override
+  void initState() {
+    super.initState();
+    // We need to avoid to add a empty text into the constructor since the [_textEditingController.addListener] callback is triggered on an empty string
+    if (widget.text.replaceAll(" ", "").isNotEmpty) {
+      final text = widget.text;
+
+      _textEditingController = TextEditingController(text: text);
+
+      // We always want the cursor at the end of the text in the initial state
+      onNextFrame(() {
+        // FROM: https://stackoverflow.com/questions/56851701/how-to-set-cursor-position-at-the-end-of-the-value-in-flutter-in-textfield
+        final selection = TextSelection.collapsed(offset: text.length);
+
+        _textEditingController.value = _textEditingController.value
+            .copyWith(text: text, selection: selection);
+      });
+    } else {
+      _textEditingController = TextEditingController();
+    }
+
+    _focusNode.addListener(() {
+      setState(() {});
+      widget.onFocusChanged?.call(_focusNode.hasFocus);
+
+      if (!_focusNode.hasFocus) {
+        widget.onLeave?.call();
+      }
+    });
+  }
+
+  @override
+  FutureOr<void> afterFirstBuild() {
+    if (widget.autofocus) {
+      _focusNode.requestFocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final style = widget.textStyle;
+
+    return TextFormField(
+      minLines: widget.minLines,
+      maxLines: widget.maxLines,
+      maxLength: widget.maxLength,
+      autofillHints: widget.autofillHints,
+      inputFormatters: widget.inputFormatter,
+      maxLengthEnforcement:
+          widget.maxLength != null ? MaxLengthEnforcement.enforced : null,
+      validator: (value) {
+        final validationValue = widget.validator?.call(value);
+
+        final isTextFieldValid = validationValue == null;
+        if (widget.onValidationChanged != null &&
+            isTextFieldValid != _textFieldIsValid) {
+          onNextFrame(() => widget.onValidationChanged!(isTextFieldValid));
+        }
+
+        _textFieldIsValid = validationValue == null;
+        return validationValue;
+      },
+      autovalidateMode: widget.autoValidate
+          ? AutovalidateMode.always
+          : AutovalidateMode.disabled,
+      textInputAction: widget.textInputAction,
+      onEditingComplete: widget.onEditingComplete,
+      keyboardType: widget.textInputType,
+      style: widget.textStyleMutator != null
+          ? widget.textStyleMutator!(style)
+          : style,
+      obscureText: widget.obscureText,
+      focusNode: _focusNode,
+      controller: _textEditingController,
+      // use onChange instead of [TextEditingController.addListener]
+      // because this will notify a text change when we loose focus
+      // when routing back. This will trigger a new search which is wrong.
+      onChanged: (string) => widget.onChanged(string),
+      decoration: widget.decoration,
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant BaseTextField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    final text = widget.text;
+
+    if (oldWidget.text != text) {
+      onNextFrame(() {
+        if (text.isEmpty) {
+          _textEditingController.clear();
+        } else {
+          final selection = _textEditingController.selection;
+
+          final isNewTextSmaller = oldWidget.text.length > text.length;
+          _textEditingController.value = _textEditingController.value.copyWith(
+            text: text,
+            selection: isNewTextSmaller
+                ? selection
+                : selection.copyWith(
+                    baseOffset: text.length,
+                    extentOffset: text.length,
+                  ),
+          );
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    // We only need to dispose the FocusNode by ourself
+    // when we create the FocusNode by ourself.
+    if (widget.focusNode == null) {
+      _focusNode.dispose();
+    }
+    _textEditingController.dispose();
+    super.dispose();
+  }
+}
