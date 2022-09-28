8ffffed83a9c84d93a151302f92b79d9eedc1b7b
Julian Bissekkou <julian.bissekkou@tapped.dev>
2022-09-28 15:22:43 +0200

    add basic tests for base_text_field package

--- packages/base_text_field/test/base_text_field_test.dart
+++ packages/base_text_field/test/base_text_field_test.dart
@@ -1,5 +1,36 @@
+import 'package:base_text_field/src/base_text_field.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'util/test_screen.dart';
+
 void main() {
-  test("todo", () {});
+  testWidgets("sets and updates the text correctly", (tester) async {
+    final screenKey = GlobalKey<TestScreenState>();
+    TestScreenState screenState() => screenKey.currentState!;
+    BaseTextFieldState textFieldState() =>
+        screenState().textFieldKey.currentState!;
+
+    // Add the TextField with and initial message and make sure
+    // that the internal TextEditingController has that value.
+    await tester.pumpWidget(
+      TestScreen(key: screenKey, initialTextFieldText: "initial"),
+    );
+    expect(textFieldState().textEditingController.text, "initial");
+
+    // Update the text property in the state and make sure the
+    // BaseTextField applies this onto the TextEditingController.
+    screenState().updateText("Updated1");
+    await tester.pumpAndSettle();
+    expect(textFieldState().textEditingController.text, "Updated1");
+
+    // Enter text manually in the BaseTextField and check if the text
+    // is set in the screen via the onChange and also in the
+    // TextEditingController.
+    var textFieldFinder = find.byKey(screenKey.currentState!.textFieldKey);
+    await tester.enterText(textFieldFinder, "Update2");
+    await tester.pumpAndSettle();
+    expect(screenState().text, "Update2");
+    expect(textFieldState().textEditingController.text, "Update2");
+  });
 }
